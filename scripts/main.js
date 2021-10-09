ef90272d9b606b02dce25e50e0c4cf231cf94679
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-10-08 23:16:04 -0500

    .

--- scripts/main.js
+++ scripts/main.js
@@ -25,5 +25,22 @@ themeToggle.addEventListener('click', () => {
     }
     localStorage.setItem('theme', theme)
 })
-
+// Swipers
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  effect: "fade",
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
