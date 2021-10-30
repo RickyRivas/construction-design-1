7b57907fc5cc3a8ebe65e15db2ec8300d75b4c32
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-10-30 14:41:43 -0500

    added services section

--- scripts/main.js
+++ scripts/main.js
@@ -91,3 +91,20 @@ var swiper = new Swiper(".swiper-container-work", {
     prevEl: ".swiper-button-prev",
   },
 });
+var swiper = new Swiper(".swiper-container-services", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
